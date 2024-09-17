1a33434f5ce118b8c2a83478038baa255b079618
Aniket Jas <aniketd232@gmail.com>
2024-09-18 00:49:28 +0530

    Auth UI complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./pages/auth";
+import Profile from "./pages/profile";
+import Chat from "./pages/chat";
 
 const App = () => {
   return (
-    <div>
-      <Button>Click Me Bro Please!</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Navigate to={"/auth"} />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
