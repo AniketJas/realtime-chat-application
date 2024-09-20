8d49110cb492b262cdcdd5ebae1a2a73b515bd83
Aniket Jas <aniketd232@gmail.com>
2024-09-20 10:25:37 +0530

    added zustand and routes update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,49 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./pages/auth";
 import Profile from "./pages/profile";
 import Chat from "./pages/chat";
+import { useAppStore } from "./store";
+
+const PrivateRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? children : <Navigate to="/auth" />;
+};
+
+const AuthRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? <Navigate to="/chat" /> : children;
+};
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<Navigate to={"/auth"} />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route
+          path="/auth"
+          element={
+            <AuthRoute>
+              <Auth />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
